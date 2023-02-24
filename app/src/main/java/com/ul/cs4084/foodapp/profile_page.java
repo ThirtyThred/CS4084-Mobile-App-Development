f959b852d0645b45be1a17d7d353165becefb14f
7LuxinC <20239696@studentmail.ul.ie>
2023-02-24 23:57:44 +0000

    Create a profile page

---
+++ app/src/main/java/com/ul/cs4084/foodapp/profile_page.java
@@ -0,0 +1,14 @@
+package com.ul.cs4084.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
