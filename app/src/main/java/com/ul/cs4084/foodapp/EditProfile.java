58992ff84436e7845ff332ae1969082d64f81afd
7LuxinC <20239696@studentmail.ul.ie>
2023-04-16 16:16:27 +0100

    update images in firebase +an image is store in profile picture

--- app/src/main/java/com/ul/cs4084/foodapp/EditProfile.java
+++ app/src/main/java/com/ul/cs4084/foodapp/EditProfile.java
@@ -1,16 +1,14 @@
 package com.ul.cs4084.foodapp;
 
-import static androidx.fragment.app.FragmentManager.TAG;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,12 +16,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,15 +35,15 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EditProfile extends AppCompatActivity {
 
-    public static  final String TAG = "TAG";
+    public static final String TAG = "TAG";
     public static final int Request_code = 100;
-    EditText p_fullname,p_phone,p_email,p_address;
+    EditText p_fullname, p_phone, p_email, p_address;
     ImageView profileImageView;
     FirebaseAuth fAuth;
     FirebaseFirestore fstore;
@@ -51,28 +52,11 @@ public class EditProfile extends AppCompatActivity {
     com.google.firebase.storage.StorageReference storageReference;
     ImageView image_View;
 
-    ActivityResultLauncher<String> galleryLauncher;
+    private ActivityResultLauncher<String> uploadImageLauncher;
+    private ActivityResultLauncher<Intent> takePictureLauncher;
 
-    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    // Handle the result from the activity here
-                    Intent data = result.getData();
-                    if(Request_code == 100){
-                        if(Request_code == Activity.RESULT_OK){
-                            Uri imageUri = data.getData();
-
-                            Bitmap image = (Bitmap) data.getExtras().get("data");
-                            image_View.setImageBitmap(image);
-
-                            uploadImageToFirebase(imageUri);
-                        }
-                    }
-                }
-            });
-
-
+    private static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
+    private ActivityResultLauncher<String> cameraPermissionLauncher;
 
 
     @Override
@@ -80,6 +64,8 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         setTitle("Edit Profile");
+
+        loadProfilePicture();
 
         Intent data = getIntent();
         String fullname = data.getStringExtra("fullname");
@@ -95,36 +81,73 @@ public class EditProfile extends AppCompatActivity {
         p_phone = findViewById(R.id.p_phoneNo);
         p_email = findViewById(R.id.p_email);
         p_address = findViewById(R.id.p_address);
-        profileImageView = findViewById(R.id.cam);
         saveBtn = findViewById(R.id.save_b);
         storageReference = FirebaseStorage.getInstance().getReference();
+
+        if (storageReference != null) {
+            Log.d(TAG, "Storage Reference is not null. Connected to Firebase Storage.");
+        } else {
+            Log.d(TAG, "Storage Reference is null. Failed to connect to Firebase Storage.");
+        }
+
+        profileImageView = findViewById(R.id.cam);
         image_View = findViewById(R.id.imageView3);
+
+        cameraPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                takePicture();
+            } else {
+                Toast.makeText(this, "permission denied..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Bundle extras = result.getData().getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Uri imageUri = getImageUri(getApplicationContext(), imageBitmap);
+                uploadImageToFirebaseStorage(imageUri);
+                image_View.setImageBitmap(imageBitmap);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(500, // width
+                        500 // height
+                );
+                params.setMargins(0, 50, 0, 0);
+                params.gravity = Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL;
+                image_View.setLayoutParams(params);
+            } else {
+                Toast.makeText(this, "image path error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        uploadImageLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
+            uploadImageToFirebaseStorage(uri);
+        });
 
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(p_fullname.getText().toString().isEmpty() || p_phone.getText().toString().isEmpty() || p_address.getText().toString().isEmpty()) {
+                if (p_fullname.getText().toString().isEmpty() || p_phone.getText().toString().isEmpty() || p_address.getText().toString().isEmpty()) {
                     Toast.makeText(EditProfile.this, "one of many field are empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                String user_email =p_email.getText().toString();
+                String user_email = p_email.getText().toString();
                 user.updateEmail(user_email).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
                         DocumentReference docRef = fstore.collection("users").document(user.getUid());
-                        Map<String,Object> edited = new HashMap<>();
-                        edited.put("Address",p_address.getText().toString() );
-                        edited.put("email",user_email);
-                        edited.put("fullname",p_fullname.getText().toString());
-                        edited.put("phoneNo",p_phone.getText().toString());
+                        Map<String, Object> edited = new HashMap<>();
+                        edited.put("Address", p_address.getText().toString());
+                        edited.put("email", user_email);
+                        edited.put("fullname", p_fullname.getText().toString());
+                        edited.put("phoneNo", p_phone.getText().toString());
 
                         docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
                                 Toast.makeText(EditProfile.this, "profile updated", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(),Profile_page.class));
+                                startActivity(new Intent(getApplicationContext(), Profile_page.class));
                                 finish();
                             }
                         });
@@ -150,7 +173,7 @@ public class EditProfile extends AppCompatActivity {
         profileImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                askCameraPermission();
+                checkCameraPermission();
 
             }
         });
@@ -163,73 +186,64 @@ public class EditProfile extends AppCompatActivity {
 
         Log.d(TAG, "onCreate:" + fullname + " " + email + " " + phone + " " + address);
     }
-/*
-    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode == 1000){
-            if(requestCode == Activity.RESULT_OK){
-                Uri imageUri = data.getData();
 
-                uploadImageToFirebase(imageUri);
-            }
+
+    private boolean checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+            return false;
         }
-
     }
-*/
-    private void uploadImageToFirebase(Uri imageUri) {
-        final StorageReference fileRef = storageReference.child("users/" + fAuth.getCurrentUser().getUid() + "/profile.jpg");
-        fileRef.putFile(imageUri)
-                .addOnSuccessListener(taskSnapshot -> {
-                    fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                        Picasso.get().load(uri).into(image_View);
-                    });
-                })
-                .addOnFailureListener(e -> {
-                    Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            takePictureLauncher.launch(takePictureIntent);
+        } else {
+            Toast.makeText(this, "Error happened", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadImageToFirebaseStorage(Uri imageUri) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imagesRef = storageRef.child("users/" + fAuth.getCurrentUser().getUid() + "/images/" + imageUri.getLastPathSegment());
+
+        imagesRef.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+            Toast.makeText(this, "Image saved", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, "Error happened", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private Uri getImageUri(Context context, Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private void loadProfilePicture() {
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imagesRef = storageRef.child("users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/images/");
+
+        imagesRef.listAll().addOnSuccessListener(listResult -> {
+            // Check if any items exist in the directory
+            if (!listResult.getItems().isEmpty()) {
+                // Get the first item in the list
+                StorageReference firstImageRef = listResult.getItems().get(0);
+
+                // Load the image into imageView3
+                firstImageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    Glide.with(this).load(uri).into(image_View);
+                }).addOnFailureListener(e -> {
+                    Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
                 });
-    }
-
-    private void askCameraPermission(){
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, Request_code);
-        }else{
-            openCamera();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == Request_code) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera();
-            } else {
-                Toast.makeText(this, "Camera Permission is Required to user camera", Toast.LENGTH_SHORT).show();
             }
-        }
-    }
-
-    private  void openCamera(){
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        someActivityResultLauncher.launch(cameraIntent);
-
-
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, "Failed to load images", Toast.LENGTH_SHORT).show();
+        });
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/ul/cs4084/foodapp/Profile_page.java
+++ app/src/main/java/com/ul/cs4084/foodapp/Profile_page.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,7 +23,6 @@ import com.ul.cs4084.foodapp.databinding.ActivityProfilePageBinding;
 
 import java.io.File;
 
-
 public class Profile_page extends DrawerBaseActivity {
     ActivityProfilePageBinding activityProfilePageBinding;
     private TextView phone, user_name, email, address;
@@ -31,6 +32,7 @@ public class Profile_page extends DrawerBaseActivity {
     private FirebaseUser user;
 
     com.google.firebase.storage.StorageReference storageReference;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +44,6 @@ public class Profile_page extends DrawerBaseActivity {
         fstore = FirebaseFirestore.getInstance();
         storageReference = FirebaseStorage.getInstance().getReference();
 
-
         Button button = findViewById(R.id.button3);
         user_name = findViewById(R.id.textView4);
         phone = findViewById(R.id.textView);
@@ -51,17 +52,10 @@ public class Profile_page extends DrawerBaseActivity {
         userId = fAuth.getCurrentUser().getUid();
         user = fAuth.getCurrentUser();
         Button c_button = findViewById(R.id.button7);
-      /* imageView = findViewById(R.id.constraintLayout3);
+        imageView = findViewById(R.id.imageView4);
 
+        loadProfilePicture();
 
-
-        com.google.firebase.storage.StorageReference profileRef = storageReference.child("users/"+fAuth.getCurrentUser().getUid()+"/profile.jpg");
-        profileRef.getDownloadUrl().addOnSuccessListener((OnSuccessListener ) (uri) ->{
-            Picasso.get().load((File) uri).into((ImageView) imageView3);
-
-
-        });
-*/
         DocumentReference documentReference = fstore.collection("users").document(userId);
         documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
             if (documentSnapshot.exists()) {
@@ -72,10 +66,9 @@ public class Profile_page extends DrawerBaseActivity {
             }
         });
 
-        c_button.setOnClickListener(new View.OnClickListener()
-        {
+        c_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view){
+            public void onClick(View view) {
                 // Create an Intent to navigate to the second activity
                 Intent intent3 = new Intent(Profile_page.this, Change_Password.class);
                 startActivity(intent3);
@@ -107,6 +100,27 @@ public class Profile_page extends DrawerBaseActivity {
         });
     }
 
+    private void loadProfilePicture() {
 
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference imagesRef = storageRef.child("users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/images/");
+
+        imagesRef.listAll().addOnSuccessListener(listResult -> {
+            // Check if any items exist in the directory
+            if (!listResult.getItems().isEmpty()) {
+                StorageReference firstImageRef = listResult.getItems().get(0);
+
+                firstImageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    Glide.with(this)
+                            .load(uri)
+                            .into(imageView);
+                }).addOnFailureListener(e -> {
+                    Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
+                });
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, "Failed to load images", Toast.LENGTH_SHORT).show();
+        });
+    }
 
 }
